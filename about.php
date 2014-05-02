a9b4b01ab7b0faf4ad8cfd5aa38dc9008fdda510
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-02 14:11:43 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,62 @@
+<?php
+	// if session not already started, begin one
+	if (!isset($_SESSION)){
+		session_start();
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="main.css" />
+<title>Humane Society Thrift Store of Fairfax</title>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+</head>
+
+
+
+<body>
+<!-- Header --!>
+<div id="header">
+		<img style="float:left" src="logo.png" alt="logo">
+		<img style="float:right" src="logo.png" alt="logo">
+		<h1 style="margin-bottom:0;text-align: center">Humane Society Thrift Store</h1>
+</div>
+
+<!-- Navigation Bar --!>
+	<div id="content">
+		<ul id="navigation">
+		    <li><a href="homepage.php">Homepage</a></li>
+		    <li><a href="announcements.php">Announcements</a></li>
+			<li><a href="about.php">About The Humane Society Thrift Store</a></li>
+			<li><a href="project1.php">Products We Have</a></li>
+			<li><a href="contact.php">Contact The Humane Society Thrift Store</a></li>
+		</ul>
+</html>		
+
+<html>
+<!-- User login --!>		
+    <form action="login.php" method="POST">
+    <fieldset>
+    <legend>Please Login</legend>   
+        <labelfor "loginID">Login</label><br/>        
+        <input type="text" name="loginID" id="loginID"><br/>
+
+        <label for "password">Password</label><br/>
+        <input type="text" name="password" id="password"><br/>
+
+        <input type="submit" value="login">
+    </fieldset>
+    </form>
+                <p>
+                        The content of this site is the original work of Lindsey Hall and Karen Bacon and
+                        intended for educational purposes.<br /> For more information visit:
+                        <a href="http://www.gmu.edu/catalog/apolicies/">
+                                http://www.gmu.edu/catalog/apolicies</a>
+                </p>
+
+ 
+</body>
+
+</html>
