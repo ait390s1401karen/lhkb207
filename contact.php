3a79dbf9068c03d9e8ec8b69fbb71fdd68526f83
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-02 14:15:43 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,10 @@
+<?php
+	// if session not already started, begin one
+	if (!isset($_SESSION)){
+		session_start();
+	}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 
       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -5,16 +12,35 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-
-   <title>Form Contact</title>
-
+<link rel="stylesheet" type="text/css" href="main.css" />
+<title>Humane Society Thrift Store of Fairfax</title>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 </head>
+
+
+
+<body>
+<!-- Header --!>
+<div id="header">
+		<img style="float:left" src="logo.png" alt="logo">
+		<img style="float:right" src="logo.png" alt="logo">
+		<h1 style="margin-bottom:0;text-align: center">Contact The Humane Society Thrift Store</h1>
+</div>
+
+<!-- Navigation Bar --!>
+	<div id="content">
+		<ul id="navigation">
+		    <li><a href="homepage.php">Homepage</a></li>
+		    <li><a href="announcements.php">Announcements</a></li>
+			<li><a href="about.php">About The Humane Society Thrift Store</a></li>
+			<li><a href="products.php">Products We Have</a></li>
+			<li><a href="contact.php">Contact The Humane Society Thrift Store</a></li>
+		</ul>
+</html>				
 
  
 
 <body>
-
- 
 
 <?php
  
