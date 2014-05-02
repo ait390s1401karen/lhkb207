326be18c9bcc225cb91e4baaef8fd25335a06f00
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-02 14:20:29 -0400

    Update products.php

--- products.php
+++ products.php
@@ -1,18 +1,40 @@
+<?php
+// if session not already started, begin one
+	if (!isset($_SESSION)){
+		session_start();
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<?php
-session_start();
-?>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 <link rel="stylesheet" type="text/css" href="main.css" />
-<title>Products</title>
+<title>Humane Society Thrift Store of Fairfax</title>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 </head>
+
+
+
 <body>
+<!-- Header --!>
+<div id="header">
+		<img style="float:left" src="logo.png" alt="logo">
+		<img style="float:right" src="logo.png" alt="logo">
+		<h1 style="margin-bottom:0;text-align: center">Humane Society Thrift Store</h1>
+</div>
+
+<!-- Navigation Bar --!>
+	<div id="content">
+		<ul id="navigation">
+		    <li><a href="homepage.php">Homepage</a></li>
+		    <li><a href="announcements.php">Announcements</a></li>
+			<li><a href="about.php">About The Humane Society Thrift Store</a></li>
+			<li><a href="products.php">Products We Have</a></li>
+			<li><a href="contact.php">Contact The Humane Society Thrift Store</a></li>
+		</ul>
+</html>		
+
 
 <?php
 
@@ -53,13 +75,30 @@ echo "</table>";
 
 mysqli_close($con);
 
-
-
-
-
-
-
 ?>
 
+<html>
+<!-- User login --!>		
+    <form action="login.php" method="POST">
+    <fieldset>
+    <legend>Please Login</legend>   
+        <labelfor "loginID">Login</label><br/>        
+        <input type="text" name="loginID" id="loginID"><br/>
+
+        <label for "password">Password</label><br/>
+        <input type="text" name="password" id="password"><br/>
+
+        <input type="submit" value="login">
+    </fieldset>
+    </form>
+                <p>
+                        The content of this site is the original work of Lindsey Hall and Karen Bacon and
+                        intended for educational purposes.<br /> For more information visit:
+                        <a href="http://www.gmu.edu/catalog/apolicies/">
+                                http://www.gmu.edu/catalog/apolicies</a>
+                </p>
+
+ 
 </body>
+
 </html>
