cab393290ab897052d43a1f1bf0a86b943889942
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-02 13:28:22 -0400

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,9 +1,10 @@
 
-
 <?php
-session_start();
+ session_start();
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!DOCTYPE html>
+<html>
 
 <head>
 <link rel="stylesheet" type="text/css" href="main.css" />
@@ -11,26 +12,49 @@ session_start();
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 </head>
 
-session_start();
+<!--session_start();-->
 
 <body>
-<h1>Humane Society Thrift Store</h1>
-<p>The Humane Society of Fairfax County depends upon the proceeds of our thrift store to assist with the substantial and ever-increasing costs of medical care for our rescued animals.
+<!-- Header --!>
+<div id="header">
+		<img style="float:left" src="logo.png" alt="logo">
+		<img style="float:right" src="logo.png" alt="logo">
+		<h1 style="margin-bottom:0;text-align: center">Humane Society Thrift Store</h1>
+</div>
+
+<!-- Navigation Bar --!>
+	<div id="content">
+		<ul id="navigation">
+		    <li><a href="homepage.html">Homepage</a></li>
+		    <li><a href="announcements.html">Announcements</a></li>
+			<li><a href="about.php">About The Humane Society Thrift Store</a></li>
+			<li><a href="project1.php">Products We Have</a></li>
+			<li><a href="contact.php">Contact The Humane Society</a></li>
+		</ul>
+
+<div id="container 1">
+	<div id="contatiner 2">
+		<p>The Humane Society of Fairfax County depends upon the proceeds of our thrift store to assist with the substantial and ever-increasing costs of medical care for our rescued animals.
 Additionally, this funding helps support the general feeding and care of the animals while they await their new loving families.
 
-When you are in the mood for Spring cleaning or if you have occasion to go through that attic or storage room, please keep us in 
-mind and donate your well-cared for wares. Household items, toys, books and [clean] clothing that can be sold without repair are greatly appreciated. 
+When you are in the mood for Spring cleaning or if you have occasion to go through that attic or storage room, please keep us in
+mind and donate your well-cared for wares. Household items, toys, books and [clean] clothing that can be sold without repair are greatly appreciated.
 And when you drop off your donations, take a moment to browse the thrift store for fine collectibles, antiques, or other treasures.
 
-We hope to see you soon at our shop! 
-</p>
+We hope to see you soon at our shop!</p>
+	
+	<div id="pic">
+			<td><img style="float:left" src=".png" alt="dog"></td>
+		</div>
 
-<h1>
-Announcements</h1>
+	</div>
+</div>
+
+<h1>Announcements</h1>
 
 <?php
 
-$con = mysqli_connect("helios.vse.gmu.edu","kb","password", "kb");
+$con = mysqli_connect("helios.vse.gmu.edu","lhall16","it207", "lhall16");
 
 
 
@@ -46,8 +70,6 @@ echo "<table border='1'>
 <th>Announcement Title</th>
 <th>Announcement</th>
 </tr> " ;
-
-
 
 
 while ($row = mysqli_fetch_array($result))
@@ -66,15 +88,7 @@ mysqli_close($con);
 
 ?>
 
-
-
-
-
-
-
-
-
-<p><a href='http://helios.ite.gmu.edu/~kbacon5/groupProject/announcements.php'>Click here for ALL Announcements</a>
+<p><a href='http://helios.ite.gmu.edu/~lhall16/group/announcements.php'>Click here for ALL Announcements</a>
 
 </p>
 
@@ -91,9 +105,6 @@ to prevent all forms of cruelty to animals, both domestic and wild, by every leg
                             <fieldset>
                                 <legend>Please Login</legend>
 
-              
-        
-        
         <labelfor "loginID">Login</label><br/>
         
         <input type="text" name="loginID" id="loginID"><br/>
@@ -107,11 +118,6 @@ to prevent all forms of cruelty to animals, both domestic and wild, by every leg
         </fieldset>
 
                 </form>
-
-
-      
-    
-
        
                 <p>
                         The content of this site is the original work of Lindsey Hall and Karen Bacon and
@@ -120,7 +126,6 @@ to prevent all forms of cruelty to animals, both domestic and wild, by every leg
                                 http://www.gmu.edu/catalog/apolicies</a>
                 </p>
 
- 
 </body>
 
 </html>
