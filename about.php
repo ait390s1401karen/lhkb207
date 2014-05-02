36e9b06ce4fe84cbfd56fa8deabbac16b3063781
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-02 14:14:36 -0400

    Update about.php

--- about.php
+++ about.php
@@ -30,7 +30,7 @@
 		    <li><a href="homepage.php">Homepage</a></li>
 		    <li><a href="announcements.php">Announcements</a></li>
 			<li><a href="about.php">About The Humane Society Thrift Store</a></li>
-			<li><a href="project1.php">Products We Have</a></li>
+			<li><a href="products.php">Products We Have</a></li>
 			<li><a href="contact.php">Contact The Humane Society Thrift Store</a></li>
 		</ul>
 </html>		
