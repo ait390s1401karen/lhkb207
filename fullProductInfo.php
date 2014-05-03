69086e1fa3d70e740dc34953851a9f94e03839f0
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-03 16:15:13 -0400

    Update fullProductInfo.php

--- fullProductInfo.php
+++ fullProductInfo.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--Displays top of the website--!>
 <?php
-session_start();
-?>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<link rel="stylesheet" type="text/css" href="main.css" />
-<title>Products</title>
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-</head>
-<body>
+	include 'topOfPage.html';
+?>	
 
 <?php
 
