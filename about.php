a871bbf7448c24bc34f3fd363f4e92779cdd805b
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-03 16:13:13 -0400

    Update about.php

--- about.php
+++ about.php
@@ -5,98 +5,22 @@
 	}
 ?>
 
-<!DOCTYPE html>
-<html>
+<!--Displays top of the website--!>
+<?php
+	include 'topOfPage.html';
+?>	
 
-<head>
-<link rel="stylesheet" type="text/css" href="main.css" />
-<title>Humane Society Thrift Store of Fairfax</title>
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-</head>
+<h2>About The Humane Society Thrift Store</h2>
 
+<div id="bodyContainer">
+<p>The Humane Society of Fairfax County depends upon the proceeds of our thrift store to assist with the substantial and ever-increasing costs of medical care for our rescued animals. Additionally, this funding helps support the general feeding and care of the animals while they await their new loving families.
 
+When you are in the mood for Spring cleaning or if you have occasion to go through that attic or storage room, please keep us in mind and donate your well-cared for wares. Household items, toys, books and [clean] clothing that can be sold without repair are greatly appreciated. And when you drop off your donations, take a moment to browse the thrift store for fine collectibles, antiques, or other treasures.
 
-<body>
-<!-- Header --!>
-<div id="header">
-		<img style="float:left" src="logo.png" alt="logo">
-		<img style="float:right" src="logo.png" alt="logo">
-		<h1 style="margin-bottom:0;text-align: center">Humane Society Thrift Store</h1>
+We hope to see you soon at our shop!</p>
 </div>
 
-<!-- Navigation Bar --!>
-	<div id="content">
-		<ul id="navigation">
-		    <li><a href="homepage.php">Homepage</a></li>
-		    <li><a href="announcements.php">Announcements</a></li>
-			<li><a href="about.php">About The Humane Society Thrift Store</a></li>
-			<li><a href="products.php">Products We Have</a></li>
-			<li><a href="contact.php">Contact The Humane Society Thrift Store</a></li>
-		</ul>
-</html>		
-
-
-
-
-<div id="login">
-<?php	
-	if (isset($_SESSION['user_email'])) {
-		if($_SESSION['user_email']=="admin"){
-			echo '<div id="sidebarUserLoggedIn">';
-				echo '<fieldset>';
-					echo '<legend>User Options</legend>';
-					echo '<a href="adminPage.php">Administration Options</a>';
-					echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-					echo '<a href="logout.php">Logout</a>';
-				echo '</fieldset>';
-				echo '</form>	';
-			echo '</div>';
-		}
-		else {	
-			echo '<div id="sidebarUserLoggedIn">';
-				echo '<fieldset>';
-					echo '<legend>User Options</legend>';
-					echo '<a href="profile.php">Profile</a>';
-					echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-					echo '<a href="logout.php">Logout</a>';
-				echo '</fieldset>';
-				echo '</form>	';
-			echo '</div>';
-		}
-
-	}
-	else {	
-			echo '<form action="login.php" method="POST">';
-			echo '<fieldset>';
-				echo '<legend>User Login</legend>';
-					echo '<label for="username">Username</label>';
-					echo ' <input type="text" name="username" id="username">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-						
-					echo '<label for="password">Password</label>';
-					echo '<input type="password" name="password" id="password">&nbsp;&nbsp;&nbsp;';
-						
-					echo '<input type="submit" name="submit" value="submit">';
-					
-					echo '<br>';
-					
-					echo '<a href="register.php">New User Registration</a>';
-			echo '</fieldset>';
-			echo '</form>	';	
-			echo '</div>';
-	}	
-?>
-</div>
-
-<html>    
-    <div id="footer">
-                <i>
-                        The content of this site is the original work of Lindsey Hall and Karen Bacon and
-                        intended for educational purposes.<br /> For more information visit:
-                        <a href="http://www.gmu.edu/catalog/apolicies/">
-                                http://www.gmu.edu/catalog/apolicies</a>
-                </i>
-	</div>
- 
-</body>
-
-</html>
+<!--Displays bottom of the website--!>
+<?php
+	include 'bottomOfPage.html';
+?>	
