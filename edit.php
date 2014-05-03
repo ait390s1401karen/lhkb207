181af5c346f995777d22edeb6a2d93de616698bb
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-03 16:14:53 -0400

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,31 +1,37 @@
-
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
 
-$usePass=$_POST['usePass'];
-$useFirst=$_POST['useFirst'];
-$useLast=$_POST['useLast'];
-$useAddress=$_POST['useAddress'];
-$useEmail=$_POST['useEmail']; 
+session_start();
 
+$useremail=$_SESSION['user_email'];
 
-$con = mysqli_connect("helios.vse.gmu.edu","kb","password", "kb");
+$con = mysqli_connect("helios.vse.gmu.edu","lhall16","it207", "lhall16");
 
-
-       
-
-$order = "UPDATE userProfiles
-SET user_email ='$useEmail', user_password='$usePass', user_firstName='$useFirst', user_lastName='$useLast', user_address='$useAddress' WHERE user_email='$useEmail'"; 
-
-
-
-
-$result1 = mysqli_query($con, $order);
+$newpassword= $_POST['password'];
+$newpassword2= $_POST['password1'];
+$newfname= $_POST['firstName'];
+$newlname=$_POST['lastName'];
+$newaddress=$_POST['address'];
+if ($newpassword == $newpassword2){
+$matchpassword=$newpassword;
+}
 
 
-mysqli_close($con);       
+$updatequery = "UPDATE userProfiles SET user_firstName='$newfname', user_lastName='$newlname', user_address='$newaddress' WHERE user_email='$useremail'";
+$updatepass = "UPDATE userProfiles SET user_password='$matchpassword' WHERE user_email='$useremail'";
 
+//Allows user to change password
+if(isset($matchpassword)){
+$uppass = mysqli_query($con, $updatepass);
+echo "Your password has been updated successfully. You will be automatically redirected to your profile page.";
+header('Refresh: 2; url=profile.php'); 
+}
 
-
-
+//Allows user to update profile
+if(isset($newfname) && isset($newlname) && isset($newaddress)){
+$upprofile = mysqli_query($con, $updatequery);
+echo "Your profile has been updated successfully.  You will be automatically redirected to your profile page.";
+header('Refresh: 2; url=profile.php'); 
+}
 
 ?>
