57793cc90de2e44be7b1697f2ab562d6c1c90343
ait390s1401karen <ait390s1401karen@gmail.com>
2014-05-03 18:44:04 -0400

    Update fullProductInfo.php

--- fullProductInfo.php
+++ fullProductInfo.php
@@ -2,72 +2,101 @@
 <?php
 	include 'topOfPage.html';
 ?>	
-
 <?php
+
+
+$rev='desc';
+$sort='product_id';
+
+//Change the value based on what its set to in the GET request
+if(isset($_GET['sort']))$sort = $_GET['sort'];
+if(isset($_GET['rev']))$rev = $_GET['rev'];
+
 
 
 $con = mysqli_connect("helios.vse.gmu.edu","kb","password", "kb");
 
-$order = 'category_id';
-
-                         
+             
 
 
 
-        $query = "SELECT category_name, product_name, product_description, product_price, product_quantity, products.category_id FROM products, category where category.category_id = products.category_id ORDER BY " . $order;
+        $query = "SELECT * from products";
 
-
-  $result = mysqli_query($con, $query);            
-
-
-
-echo "<table border='1'>
-<tr>
-<th><a href='?orderBy=category_id'>Category ID</a></th>
-<th><a href='?orderBy=category_name'>Category Name</a></th>
-<th><a href='?orderBy=product_name'>Product Name</a></th>
-<th><a href='?orderBy=product_description'>Product Description</a></th>
-<th><a href='?orderBy=product_price'>Price</a></th>
-<th><a href='?orderBy=product_quantity'>Quantity</a></th>
-
-
-</tr> " ;
-
-
-
-
-$orderBy = array('category_id', 'category_name', 'product_name', 'product_description', 'product_price', 'product_quantity');
-
-$order = 'category_id';
-if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {
-    $order = $_GET['orderBy'];
+if ($sort == 'product_id'){
+    $query .= " ORDER BY product_id";
+}
+elseif ($sort == 'product_name'){
+    $query .= " ORDER BY product_name";
+}
+elseif ($sort == 'product_description'){
+    $query .= " ORDER BY product_description";
+}
+elseif($sort == 'product_price'){
+    $query .= " ORDER BY product_price";
+}
+elseif($sort == 'product_quantity'){
+    $query .= " ORDER BY product_quantity";
+}
+elseif($sort == 'category_id'){
+    $query .= " ORDER BY category_id";
 }
 
 
 
- $query = "SELECT category_name, product_name, product_description, product_price, product_quantity, products.category_id FROM products, category where category.category_id = products.category_id ORDER BY " . $order;
+
+//Check and switch the sorting order
+if($rev == 'asc') {
+$query .= ' desc';
+$rev = 'desc';
+}
+else {
+$query .= ' asc';
+$rev = 'asc';
+}
+
+
+echo '<table border="1">';
+echo '<tr>';
+echo '<th><a href="fullProductInfo.php?sort=product_id&rev=' . $rev . '"> &nbsp;Product ID&nbsp;</a></th>';
+echo '<th><a href="fullProductInfo.php?sort=product_name&rev=' . $rev . '">&nbsp;Product Name&nbsp;</a></th>';
+echo '<th><a href="fullProductInfo.php?sort=product_description&rev=' . $rev . '">&nbsp;Description&nbsp;</a></th>';
+echo '<th><a href="fullProductInfo.php?sort=product_price&rev=' . $rev . '">&nbsp; Price&nbsp;</a></th>';
+echo '<th><a href="fullProductInfo.php?sort=product_quantity&rev=' . $rev . '">&nbsp;Quantity&nbsp;</a></th>';
+echo '<th><a href="fullProductInfo.php?sort=category_id&rev=' . $rev . '">&nbsp;Category ID&nbsp;</a></th>';
+
+
+
+
+
+
+echo '</tr>';
+
+
+
 
 
 $result = mysqli_query($con, $query);
 
 
+// While loop to populate the different results
+while ($row = mysqli_fetch_array($result)) {
+//Create the table and place contents form the database in the appropriate places
+    echo '<tr class="results">';
+    echo '<td valign="top" width="8%">' . $row['product_id'] . '</td>';
+    echo '<td valign="top" width="10%">' . $row['product_name'] . '</td>';
+    echo '<td valign="top" width="11%">' . $row['product_description'] . '</td>';
+    echo '<td valign="top" width="10%">' . $row['product_price'] . '</td>';
+    echo '<td valign="top" width="6%">' . $row['product_quantity'] . '</td>';
+  echo '<td valign="top" width="6%">' . $row['category_id'] . '</td>';
 
-while ($row = mysqli_fetch_array($result))
 
- {
 
-echo "<tr>";
-echo "<td>" . $row['category_id'] . "</td>";
-echo "<td>" . $row['category_name'] . "</td>";
-echo "<td>" . $row['product_name'] . "</td>";
 
-echo "<td>" . $row['product_description'] . "</td>";
-echo "<td>" . $row['product_price'] . "</td>";
-echo "<td>" . $row['product_quantity'] . "</td>";
+    echo '</tr>';
+  
+  }
+echo '</table>';
 
-echo "</tr>";
-}
-echo "</table>";
 
 
 
