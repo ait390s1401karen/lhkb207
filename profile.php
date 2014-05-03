40a6cf57a7670dac3e41e6eca8ce90320cd8fbed
lhall16 <lhall16@masonlive.gmu.edu>
2014-05-03 16:16:45 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,44 +1,26 @@
+<!--Displays top of the website--!>
 <?php
-
-
-
-
-$con = mysqli_connect("helios.vse.gmu.edu","kb","password", "kb");
-
-
-
-
+	include 'topOfPage.html';
 
 session_start();
-    
 
+$con = mysqli_connect("helios.vse.gmu.edu","lhall16","it207", "lhall16");
 
+    $user = $_SESSION['user_email'];
 
-
-    $user = $_POST['loginID'];
-    $pass = $_POST['password'];
 
  
-$query1 = "SELECT * FROM userProfiles WHERE user_email = '".$user."' AND user_password = '".$pass."'";
+$query = "SELECT * FROM userProfiles WHERE user_email = '$user'";
 
-$result = mysqli_query($con,$query1);
+$result = mysqli_query($con,$query);
 
-
-if ($result)
-{
-$_SESSION['logged'] = $user;
 
    echo "<html>";
-    echo "<h1>";
-   echo "Edit your Profile information " . $_SESSION['logged'];
-     echo "</h1>";
+    echo "<h2>";
+   echo "Edit " . $_SESSION['user_email'] . "Profile information ";
+     echo "</h2>";
 
 
-
-                         
-$query = "SELECT *  FROM userProfiles WHERE user_email = '" . $user ."'";
-
- $result1 = mysqli_query($con, $query);            
 
 
 echo "<table border='1'>";
@@ -46,50 +28,53 @@ echo "<form method='post' action='edit.php'>
 
 
 <tr>
-<th>EMAIL </th>
-<th>PASSWORD</th>
 <th>FIRST NAME</th>
 <th>LAST NAME</th>
 <th>ADDRESS</th>
 
 </tr> " ;
 
-
-
-while ($row = mysqli_fetch_array($result1))
-
+while ($row = mysqli_fetch_array($result))
  {
 
 echo "<tr>";
-echo "<td><input type='text' name='useEmail' size='20' value=" . $row['user_email'] . "></td>";
-echo "<td><input type='text' name='usePass' size='20' value=" . $row['user_password'] . "></td>";
-echo "<td><input type='text' name='useFirst' size='50' value="   . $row['user_firstName'] ."> </td>";
+echo "<td><input type='text' name='firstName' size='50' value="   . $row['user_firstName'] ."> </td>";
 
-echo "<td><input type='text' name='useLast' size='50' value="  . $row['user_lastName'] . "></td>";
-echo "<td> <input type='text' name='useAddress' size='80' value="  . $row['user_address'] . "></td>";
-echo "<input type='submit' name='submit' value='Update'>";
+echo "<td><input type='text' name='lastName' size='50' value="  . $row['user_lastName'] . "></td>";
+echo '<td> <input type="text" name="address" size="80" value="'  . $row['user_address'] . '"></td>';
 
 
 echo "</tr>";
 }
 echo "</table>";
+echo "<input type='submit' name='submit' value='Update Profile'>";
+echo "</form>";
 
 mysqli_close($con);
 
+echo "<br>";
+echo "<br>";
+
+echo "<table border='1'>";
+echo "<form method='post' action='edit.php'>
 
 
-}
+<tr>
+<th>PASSWORD</th>
+<th>Verify PASSWORD</th>
 
-else{
-echo "Invalid login";
+</tr> " ;
+echo "<td> <input type='password' name='password' size='80' value=''></td>";
+echo "<td> <input type='password' name='password1' size='80' value=''></td>";
 
-}
-
-
-
-
-
-   
-  
+echo "</tr>";
+echo "</table>";
+echo "<input type='submit' name='submit1' value='Change Password'>";
+echo "</form>";
    ?>
 
+<!--Displays bottom of the website--!>
+<?php
+	include 'bottomOfPage.html';
+?>	
+</html>
